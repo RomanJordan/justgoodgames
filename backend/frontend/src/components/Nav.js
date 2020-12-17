c9f7f769ad2345b0650c2297d77fb6ee1618ff75
J <Jordan.Roman90@gmail.com>
2020-12-16 21:28:16 -0500

    dark mode testing

--- backend/frontend/src/components/Nav.js
+++ backend/frontend/src/components/Nav.js
@@ -3,9 +3,16 @@ import { Link } from "react-router-dom"
 
 
 export default class Nav extends Component {
-    // constructor(props) {
-    //     const darkMode = useDarkMode(initialState, darkModeConfig);
-    // }
+    
+    constructor(props) {
+        super(props);
+        // This binding is necessary to make `this` work in the callback    
+        this.handleClick = this.handleClick.bind(this);  
+    }
+
+    handleClick() {    
+        
+    }
 
     render() {
         return(
@@ -14,7 +21,7 @@ export default class Nav extends Component {
                 <p className="navbar-brand mb-0" > <Link to="/" id="brand">Just Good Games</Link> </p>
                 <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="#">Dark Mode</a>
+                <a class="nav-link" href="#" onClick={this.handleClick}>Dark Mode</a>
             </li>
         </ul>
             </nav>
