629b6db2f8306de4b3f8502ac9f44f6e63f5d423
J <Jordan.Roman90@gmail.com>
2020-12-16 21:04:13 -0500

    dark mode testing

--- backend/frontend/src/components/Nav.js
+++ backend/frontend/src/components/Nav.js
@@ -12,7 +12,12 @@ export default class Nav extends Component {
             <nav class="navbar navbar-expand-lg navbar-light bg-lighter"  style={{borderBottom: '1px solid black'}}>
                 {/* <p class="navbar-brand mb-0">Just Good Games</p> */}
                 <p className="navbar-brand mb-0" > <Link to="/" id="brand">Just Good Games</Link> </p>
-                
+                <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Dark Mode</a>
+            </li>
+            
+        </ul>
             </nav>
         )
     }
